2724b0f86409e6116377fc9daf956d992899a5ae
Asial Jim <asialjim@hotmail.com>
2025-04-08 09:18:07 +0800

    init

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-username=$1
-password=$2
-host=$3
-port=$4
-ssh -p $port $username:$password@$host "sh '/vol2/@development/Gateway/start.sh\"
--- start.sh
+++
@@ -1,1 +0,0 @@
-sh deploy.sh asialjim xmm@0127 www.asialjim.cn 40022
